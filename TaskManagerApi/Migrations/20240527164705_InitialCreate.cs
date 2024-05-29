509f2b89551af9d50a6fe81a20936e7c46c7fdd9
NeedCookies <emilkalim895@gmail.com>
2024-05-29 08:06:58 +0300

    Add migrations

---
+++ TaskManagerApi/Migrations/20240527164705_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace TaskManagerApi.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    Description = table.Column<string>(type: "character varying(1024)", maxLength: 1024, nullable: false),
+                    Type = table.Column<string>(type: "text", nullable: false),
+                    IsCompleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tasks");
+        }
+    }
+}
---
+++ TaskManagerApi/Repository/TaskManagerRepository.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskManagerApi.Abstractions;
+using TaskManagerApi.Models;
+
+namespace TaskManagerApi.Repository
+{
+    public class TaskManagerRepository : ITaskManagerRepository
+    {
+        private readonly TaskManagerDbContext _dbContext;
+
+        public TaskManagerRepository(TaskManagerDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<TaskItem>> Get()
+        {
+            return await _dbContext.Tasks
+                .AsNoTracking()
+                .OrderBy(task => task.Name)
+                .ToListAsync();
+        }
+
+        public async Task<TaskItem?> GetById(int id)
+        {
+            return await _dbContext.Tasks
+                .AsNoTracking()
+                .FirstOrDefaultAsync(task => task.Id == id);
+        }
+
+        public async Task CreateTask(string name, string description, string type, bool isCompleted)
+        {
+            var taskItem = new TaskItem();
+            taskItem.Name = name;
+            taskItem.Description = description;
+            taskItem.Type = type;
+            taskItem.IsCompleted = isCompleted;
+            await _dbContext.Tasks.AddAsync(taskItem);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateTaskById(int id, string name, string description, string type, bool isCompleted)
+        {
+            var taskItem = await _dbContext.Tasks.FirstOrDefaultAsync(task => task.Id == id)
+                ?? throw new NullReferenceException();
+
+            taskItem.Name = name;
+            taskItem.Description = description;
+            taskItem.Type = type;
+            taskItem.IsCompleted = isCompleted;
+
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteTaskById(int id)
+        {
+            var taskitem = _dbContext.Tasks.FirstOrDefault(task => task.Id == id)
+                ?? throw new NullReferenceException();
+
+            _dbContext.Tasks.Remove(taskitem);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
